37cb04cd713bbf114b74aa757e696c9eeef1caf9
ynzhao96 <zhaoyn96@gmail.com>
2024-12-04 17:30:11 -0400

    add demo page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,34 +1,12 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import Displayer from "@/components/displayer";
 
 export default function Home() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
-  const handleSubmit = async () => {
-    const response = await fetch("/api/hello", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ test: "Hello from frontend!" }),
-    });
-    const data = await response.json();
-    console.log(data);
-  };
-
   return (
     <>
       <div>About Page</div>
-      <div>App API Response: {message}</div>
-      <button onClick={handleSubmit}>submit</button>
-      <div>Test POST Request</div>
+      <Displayer />
     </>
   );
 }
